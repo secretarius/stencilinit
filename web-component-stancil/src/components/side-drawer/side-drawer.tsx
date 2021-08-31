7e04fd0baf613f0babb25008c98dcb5b47357cc6
Alex <olexandrmai@gmail.com>
2021-08-31 17:15:38 +0300

    stencil init

---
+++ web-component-stancil/src/components/side-drawer/side-drawer.tsx
@@ -0,0 +1,17 @@
+import {Component, h} from '@stencil/core';
+
+@Component({
+    tag: 'uc-side-drawer',
+    styleUrl: './side-drawer.css',
+    shadow: true
+})
+
+export class SideDrawer {
+    render() {
+        return (
+            <aside>
+                <h1>The Side Drawer</h1>
+            </aside>
+        );
+    }
+}
